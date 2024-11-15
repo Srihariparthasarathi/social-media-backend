5253d16dd6a40c1158a1425b545dc9acf1aa0a20
Srihariparthasarathi <srihariparthasarathi20@gmail.com>
2024-11-15 17:28:50 +0530

    create functionality to get post data by ID using getPostById method, add getByPostId in PostModel to return post and error handling it, update swagger document for GET /api/product/:id

--- src/features/posts/posts.controller.js
+++ src/features/posts/posts.controller.js
@@ -1,21 +1,18 @@
-import ApplicationError from "../../middlewares/applicationError.middleware.js";
 import PostsModel from "./posts.model.js";
 
-const NO_POST_IN_APPLICATION = "There are no posts yet. Be the first to share and make your mark!";
-
 const RETURN_POST_SUCCESS_CODE = 200;
-const POSTS_UNAVAILABLE_CODE = 404;
-
 
 export default class PostsController{
 
     getAllPosts(req, res){
         const posts = PostsModel.getAll();
-        if(posts.length == 0) throw new ApplicationError(NO_POST_IN_APPLICATION, POSTS_UNAVAILABLE_CODE);
         res.status(RETURN_POST_SUCCESS_CODE).json({posts: posts});
     }
 
     getPostById(req, res){
+        const postId = req.params["id"];
+        const post = PostsModel.getByPostId(postId);
+        res.status(RETURN_POST_SUCCESS_CODE).json({post: post});
 
     }
     getPostByUser(req, res){
--- src/features/posts/posts.model.js
+++ src/features/posts/posts.model.js
@@ -1,3 +1,10 @@
+import ApplicationError from "../../middlewares/applicationError.middleware.js";
+
+const NO_POST_IN_APPLICATION = "There are no posts yet. Be the first to share and make your mark!";
+const POST_NOT_FOUND = "Please check the post ID and try again. No post found with ID:";
+
+const POST_NOT_FOUND_CODE = 404;
+
 
 export default class PostsModel{
     constructor(id, userid, caption, imageurl){
@@ -8,7 +15,14 @@ export default class PostsModel{
     }
 
     static getAll(){
+        if(postList.length == 0) throw new ApplicationError(NO_POST_IN_APPLICATION, POST_NOT_FOUND_CODE);
         return postList;
+    }
+
+    static getByPostId(postId){
+        const post = postList.find((post) => post.id == postId);
+        if(!post) throw new ApplicationError(`${POST_NOT_FOUND} ${postId}`, POST_NOT_FOUND_CODE);
+        return post;
     }
 }
 
