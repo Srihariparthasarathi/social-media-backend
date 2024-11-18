10f24b37b44e1576c7557946b46f724a9aa829d0
Srihariparthasarathi <srihariparthasarathi20@gmail.com>
2024-11-18 09:36:36 +0530

    configer bookmarks router

--- api.router.js
+++ api.router.js
@@ -5,6 +5,7 @@ import userRouter from "./src/features/users/users.router.js";
 import postsRouter from "./src/features/posts/posts.router.js";
 import commentsRouter from "./src/features/comments/comments.router.js";
 import likesRouter from "./src/features/likes/likes.router.js"
+import bookmarksRouter from "./src/features/bookmarks/bookmarks.router.js"
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.use("/users", userRouter);
 router.use("/posts", postsRouter);
 router.use("/comments", commentsRouter);
 router.use("/likes", likesRouter);
+router.use("/bookmarks", bookmarksRouter);
 
 
 
