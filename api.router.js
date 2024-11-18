5e86b4bb6e37d061ab6e627a5b3d363c57b445a7
Srihariparthasarathi <srihariparthasarathi20@gmail.com>
2024-11-18 05:54:05 +0530

    add basic configration and routers in likes.router.js and add middleware in api.router.js to handle /likes

--- api.router.js
+++ api.router.js
@@ -4,13 +4,14 @@ import express from "express";
 import userRouter from "./src/features/users/users.router.js";
 import postsRouter from "./src/features/posts/posts.router.js";
 import commentsRouter from "./src/features/comments/comments.router.js";
+import likesRouter from "./src/features/likes/likes.router.js"
 
 const router = express.Router();
 
 router.use("/users", userRouter);
 router.use("/posts", postsRouter);
 router.use("/comments", commentsRouter);
-
+router.use("/likes", likesRouter);
 
 
 
